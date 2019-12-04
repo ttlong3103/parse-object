02a769d81992284ceb448a3f627a608189113583
tran thanh long <ttlong3103>
2019-12-04 22:23:36 +0700

    implement parse number

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-module.exports = {}
+var Parser = require("jison").Parser;
+
+var grammar = {
+  lex: {
+    rules: [
+      ["\\s+", "/* skip whitespace */"],
+      /** NUMBER
+       * Support "1" , "-1" , "1.0" , "-1.0" , "1." , ".1"
+       */
+      ["[-]?((\\d+(\\.\\d*)?)|(\\d*\\.\\d+))", 'return "NUMBER";'],
+      ["$", 'return "EOF";']
+    ]
+  },
+  bnf: {
+    entry: [["expr EOF", "return $1"]],
+    expr: [["number", "$$ = $1"]],
+    number: [["NUMBER", "$$ = Number($1)"]]
+  }
+};
+
+var parser = new Parser(grammar);
+function parse(txt) {
+  return parser.parse(txt);
+}
+
+module.exports = parse;
