b283230500abaf4c868393c151f864c8b0414d6a
tran thanh long <ttlong3103>
2019-12-06 22:58:13 +0700

    implement parse single quote string

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var grammar = {
   lex: {
     // https://stackoverflow.com/questions/25889540/jison-start-conditions-with-json-format
     startConditions: {
-      DOUBLE_QUOTE_STRING_START_CONDITION: '// string starts with double quote'
+      DOUBLE_QUOTE_STRING_START_CONDITION: '// string starts with double quote',
+      SINGLE_QUOTE_STRING_START_CONDITION: '// string starts with single quote',
     },
     rules: [
       ['\\s+', '/* skip whitespace */'],
+      /** DOUBLE QUOTE STRING
+       * Note: visual "\n" we see in console when call JSON.stringify("a\nb") actually is "a" + "\" + "n" + "b"
+       */
       ['"', 'this.begin("DOUBLE_QUOTE_STRING_START_CONDITION"); return "\\"";'],
       [
         ['DOUBLE_QUOTE_STRING_START_CONDITION'],
@@ -18,6 +22,19 @@ var grammar = {
         ['DOUBLE_QUOTE_STRING_START_CONDITION'],
         '"',
         'this.popState(); return "\\"";'
+      ],
+      /** SINGLE QUOTE STRING
+       */
+      ['\'', 'this.begin("SINGLE_QUOTE_STRING_START_CONDITION"); return "\'";'],
+      [
+        ['SINGLE_QUOTE_STRING_START_CONDITION'],
+        '[^\'\\r\\n]+',
+        'return "SINGLE_QUOTE_STRING";'
+      ],
+      [
+        ['SINGLE_QUOTE_STRING_START_CONDITION'],
+        '\'',
+        'this.popState(); return "\'";'
       ],
       /** NUMBER
        * Support "1" , "-1" , "1.0" , "-1.0" , "1." , ".1"
@@ -34,17 +51,17 @@ var grammar = {
     ],
     number: [['NUMBER', '$$ = Number($1)']],
     string: [
-      [`double_quote_string`, '$$ = $1']
-      // [`single_quote_string`, '$$ = $1']
+      ['double_quote_string', '$$ = $1'],
+      ['single_quote_string', '$$ = $1']
     ],
     double_quote_string: [
       [`" "`, '$$ = ""'],
       [`" DOUBLE_QUOTE_STRING "`, '$$ = $2']
-    ]
-    // single_quote_string: [
-    //   [`' '`, '$$ = ""'],
-    //   [`' SINGLE_QUOTE_STRING '`, '$$ = $2']
-    // ],
+    ],
+    single_quote_string: [
+      [`' '`, '$$ = ""'],
+      [`' SINGLE_QUOTE_STRING '`, '$$ = $2']
+    ],
   }
 };
 
