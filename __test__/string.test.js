ef3554949e47d63d077a9adccc778a5b634a6d2d
tran thanh long <ttlong3103>
2019-12-06 23:13:34 +0700

    add more tests

--- __test__/string.test.js
+++ __test__/string.test.js
@@ -20,9 +20,28 @@ test('Double quote string with visual \\n', () => {
   expect(parse(`"abc\\n123"`)).toBe('abc\\n123');
 });
 
-test('Double quote contains single quotes', () => {
+test('Double quote string contains single quotes', () => {
   expect(parse(`"'abc'"`)).toBe(`'abc'`);
 });
+
+test('Invalid double quote string 1', () => {
+  expect(() => {
+    parse(`"missing closing quote`)
+  }).toThrow();
+});
+
+test('Invalid double quote string 2', () => {
+  expect(() => {
+    parse(`"the" hell???`)
+  }).toThrow();
+});
+
+test('Invalid double quote string 3', () => {
+  expect(() => {
+    parse(`missing opening quote"`)
+  }).toThrow();
+});
+// -----------------------------------------------------
 
 test('Single quote string 1', () => {
   expect(parse(`'abc'`)).toBe('abc');
@@ -44,6 +63,24 @@ test('Single quote string with visual \\n', () => {
   expect(parse(`'abc\\n123'`)).toBe('abc\\n123');
 });
 
-test('Single quote contains double quotes', () => {
+test('Single quote string contains double quotes', () => {
   expect(parse(`'"abc"'`)).toBe(`"abc"`);
+});
+
+test('Invalid single quote string 1', () => {
+  expect(() => {
+    parse(`'missing closing quote`)
+  }).toThrow();
+});
+
+test('Invalid single quote string 2', () => {
+  expect(() => {
+    parse(`'the' hell???`)
+  }).toThrow();
+});
+
+test('Invalid single quote string 3', () => {
+  expect(() => {
+    parse(`missing opening quote'`)
+  }).toThrow();
 });
