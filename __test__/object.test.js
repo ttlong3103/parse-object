79a2488656abc1a5cace3b402132d3874fd742ce
tran thanh long <ttlong3103>
2019-12-07 11:51:53 +0700

    implement parse object and add tests

---
+++ __test__/object.test.js
@@ -0,0 +1,59 @@
+const parse = require('..');
+
+test('Empty object', () => {
+  expect(parse(`{}`)).toEqual({});
+});
+
+test('Object with string key 1', () => {
+  expect(parse(`{"key": 1}`)).toEqual({ key: 1 });
+});
+
+test('Object with string key 2', () => {
+  expect(parse(`{'key': 1}`)).toEqual({ key: 1 });
+});
+
+test('Object with identifier key', () => {
+  expect(parse(`{key: 1}`)).toEqual({ key: 1 });
+});
+
+test('Object with all type of values and new line', () => {
+  expect(
+    parse(`{
+      single_string: 'single',
+      doubleString: "   ",
+      positive_integer: 100,
+      negative_integer: -99,
+      positive_float: 15.9999,
+      negetive_float: -123.1231234,
+      array: [1, 2, 3],
+      nested_array: [[1], [2, [3, 4]]],
+      nested_object: {
+        attr1: 1,
+        attr2: '123',
+        attr3: {}
+      }
+    }`)
+  ).toEqual({
+    single_string: 'single',
+    doubleString: "   ",
+    positive_integer: 100,
+    negative_integer: -99,
+    positive_float: 15.9999,
+    negetive_float: -123.1231234,
+    array: [1, 2, 3],
+    nested_array: [[1], [2, [3, 4]]],
+    nested_object: {
+      attr1: 1,
+      attr2: '123',
+      attr3: {}
+    }
+  });
+});
+
+test('Check ignore white spaces', () => {
+  expect(parse(`{a :1,         b: 2     , c: {                      }}`)).toEqual({
+    a: 1,
+    b: 2,
+    c: {}
+  });
+});
